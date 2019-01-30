5c8e8b91525285cd6e2bab833e928973dad2b022
Ysgard <ysgard@gmail.com>
2019-01-30 11:03:04 -0800

    emacs: explicitely set snippets dir

--- emacs.d/misc/ys-snippet.el
+++ emacs.d/misc/ys-snippet.el
@@ -7,6 +7,7 @@
 (use-package yasnippet
   :config
   ;; personal snippets dir
+  (setq yas-snippet-dirs (append yas-snippet-dirs "~/.emacs.d/snippets"))
   (yas-global-mode 1))
 
 (use-package yasnippet-snippets)
